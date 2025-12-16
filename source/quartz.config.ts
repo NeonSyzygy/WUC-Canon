8869b7dcc6858acb493eba54970ac714c649756b
Patrick McCaffrey <1471088+McCaffeteria@users.noreply.github.com>
2025-12-16 01:27:02 -0500

    Update quartz.config.ts Site Name

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "DDC Lore Vault",
+    pageTitle: "WUC Lore Vault",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
